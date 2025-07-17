db4d3cab3a2af05fa20bcfb3f4f28182a35c1730
brein594 <brein594@gmail.com>
2025-07-17 13:52:12 +0300

    dinamics addres

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,6 +1,7 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+//import io.javalin.http.NotFoundResponse;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -16,6 +17,32 @@ public class HelloWorld {
             var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result("Hello, " + name);
         });
+
+        app.get("/users/{id}/post/{postid}", ctx -> {
+            var id = ctx.pathParamAsClass("id", Integer.class);
+            var postid = ctx.pathParam("postid");
+            ctx.result("user ID: " + id + " post ID: " + postid);
+        });
+
+        /*
+        app.get("/courses/{id}", ctx -> {  //динамический маршрут
+            ctx.result("Course ID: " + ctx.pathParam("id"));
+        });
+        app.get("/users/{id}", ctx -> {
+            ctx.result("User ID: " + ctx.pathParam("id"));
+        });
+
+         */
         app.start(7070); // Стартуем веб-сервер
+
+
     }
+/*
+    public static void show(Context ctx){
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var user = UserRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Entity with id = " + id + "not found"));
+    }
+
+ */
 }
