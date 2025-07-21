af97e3cd02a6cf257ff89341280aef82b29b860f
brein594 <brein594@gmail.com>
2025-07-21 21:57:36 +0300

    jte + if

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -3,11 +3,20 @@ package org.example.hexlet;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import static io.javalin.rendering.template.TemplateUtil.model;
+
+import org.example.hexlet.dto.courses.CoursesPage;
 import org.example.hexlet.model.Course;
 import org.example.hexlet.dto.courses.CoursePage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloWorld {
     public static void main(String[] args) {
+        var oneCourse = new Course("1", "oneCourse");
+        var twoCourse = new Course("2", "twoCourse");
+
+
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte());
@@ -15,9 +24,20 @@ public class HelloWorld {
 
         app.get("/courses/{id}", ctx -> {
             var id = ctx.pathParam("id");
-            var course = new Course("1", "oneCourse");
-            var page = new CoursePage(course);
+            var page = new CoursePage(oneCourse);
             ctx.render("courses/show.jte", model("page", page));
+        });
+
+
+
+        app.get("/courses", ctx -> {
+            //var courses = /* Список курсов извлекается из базы данных */
+            var courses = new ArrayList<Course>();
+            courses.add(oneCourse);
+            courses.add(twoCourse);
+            var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/index.jte", model("page", page));
         });
 
         app.get("/", ctx -> ctx.result("Hello World"));
@@ -34,6 +54,6 @@ public class HelloWorld {
             ctx.result("user ID: " + id + " post ID: " + postid);
         });
 
-        app.start(7070);
+        app.start(7071);
     }
 }
