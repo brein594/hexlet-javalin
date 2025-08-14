e9707994b568ccd9b24ef269afe50e7f55e0f6bf
brein594 <brein594@gmail.com>
2025-08-14 16:34:13 +0300

    add plugin shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("java")
     id("io.freefair.lombok") version "8.14"
     application
